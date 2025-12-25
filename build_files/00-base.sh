f1d055e3698241fd67f3b00c1d1626b742da256c
certifiedfooliolol <mastersvanity@gmail.con>
2025-12-25 17:48:34 +0000

    fix: use CachyOS kernel

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -6,6 +6,6 @@ set -ouex pipefail
 # initialize database and install base packages
 pacman -Syu --noconfirm
 pacman -Sy --noconfirm reflector
-pacman -Sy --noconfirm base dracut linux linux-firmware ostree btrfs-progs e2fsprogs xfsprogs dosfstools skopeo dbus dbus-glib glib2 ostree shadow && pacman -S --clean --noconfirm
+pacman -Sy --noconfirm base dracut linux-cachyos-bore linux-firmware ostree btrfs-progs e2fsprogs xfsprogs dosfstools skopeo dbus dbus-glib glib2 ostree shadow && pacman -S --clean --noconfirm
 
 echo "::endgroup::"
