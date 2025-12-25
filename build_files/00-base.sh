78609e207db11f6d437ee361ac0733e350be814a
certifiedfooliolol <mastersvanity@gmail.con>
2025-12-25 17:24:30 +0000

    fix: workaround for Cachy's docker being buggy now obsolete

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -3,12 +3,9 @@
 
 set -ouex pipefail
 
-# FIXME | Fix an issue with Cachy's docker, pacman -syu fails otherwise https://discuss.cachyos.org/t/cant-update-because-of-linux-firmware-notice/19835
-mkdir /usr/lib/sysimage/lib/pacmanlocal -p
-
 # initialize database and install base packages
 pacman -Syu --noconfirm
 pacman -Sy --noconfirm reflector
-pacman -Sy --noconfirm base dracut linux-cachyos-bore linux-firmware ostree btrfs-progs e2fsprogs xfsprogs dosfstools skopeo dbus dbus-glib glib2 ostree shadow && pacman -S --clean --noconfirm
+pacman -Sy --noconfirm base dracut linux linux-firmware ostree btrfs-progs e2fsprogs xfsprogs dosfstools skopeo dbus dbus-glib glib2 ostree shadow && pacman -S --clean --noconfirm
 
 echo "::endgroup::"
