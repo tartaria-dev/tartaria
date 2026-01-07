babbd7a4a45b1ee940ccb3c4636639d61003d31f
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-01-06 18:35:27 -0800

    feat(zram): enable compression + disable zswap

--- build_files/04-misc.sh
+++ build_files/04-misc.sh
@@ -20,9 +20,13 @@ echo -e 'net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr' > /etc/sysctl.d/99-bbr3.conf
 
 # setup zram
-echo -e '[zram0]\nzram-size = min(ram, 8192)' > /usr/lib/systemd/zram-generator.conf
+echo -e '[zram0]\nzram-size = min(ram, 8192)\ncompression-algorithm = zstd\nswap-priority = 100' > /usr/lib/systemd/zram-generator.conf
+echo "vm.swappiness=10" > /etc/sysctl.d/99-zram-mem.conf
+echo 'w /sys/module/zswap/parameters/enabled - - - - 0' > /etc/tmpfiles.d/disable-zswap.conf
+
+# setup systemd-resolved
 echo -e 'enable systemd-resolved.service' > /usr/lib/systemd/system-preset/91-resolved-default.preset
-echo -e 'L /etc/resolv.conf - - - - ../run/systemd/resolve/stub-resolv.conf' > /usr/lib/tmpfiles.d/resolved-default.conf
+echo -e 'L+ /etc/resolv.conf - - - - ../run/systemd/resolve/stub-resolv.conf' > /usr/lib/tmpfiles.d/resolved-default.conf
 systemctl preset systemd-resolved.service
 
 # refresh font cache
