c25be19726c794bee301881be919cb0c0bffbd81
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-01-18 15:58:15 -0800

    fix(build): remove libfakeroot error noise by setting LD_PRELOAD

--- build_files/04-subsystem.sh
+++ build_files/04-subsystem.sh
@@ -8,26 +8,29 @@ set -ouex pipefail
 # get fakeroot
 pacman -S --noconfirm fakeroot
 
+# configure environment
+LIBFAKEROOT=$(find /usr/lib -name "libfakeroot.so" | head -n 1)
+
 # download and extract arch rootfs tarball
 curl -JLO https://archive.archlinux.org/iso/2026.01.01/archlinux-bootstrap-x86_64.tar.zst
-fakeroot tar --numeric-owner -xpf archlinux-bootstrap-x86_64.tar.zst
+LD_PRELOAD="$LIBFAKEROOT" fakeroot tar --numeric-owner -xpf archlinux-bootstrap-x86_64.tar.zst
 mv root.x86_64 /rootfs
 
 # initialize pacman keys/mirrors in rootfs
 echo "Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch" > /rootfs/etc/pacman.d/mirrorlist
-fakeroot pacman-key --gpgdir /rootfs/etc/pacman.d/gnupg --init
-fakeroot pacman-key --gpgdir /rootfs/etc/pacman.d/gnupg \
+LD_PRELOAD="$LIBFAKEROOT" fakeroot pacman-key --gpgdir /rootfs/etc/pacman.d/gnupg --init
+LD_PRELOAD="$LIBFAKEROOT" fakeroot pacman-key --gpgdir /rootfs/etc/pacman.d/gnupg \
                     --config /rootfs/etc/pacman.conf \
                     --populate archlinux
 
 # update rootfs and install base packages
-fakeroot pacman -r /rootfs -Sy --noconfirm
-fakeroot pacman -r /rootfs -S --noconfirm \
+LD_PRELOAD="$LIBFAKEROOT" fakeroot pacman -r /rootfs -Sy --noconfirm
+LD_PRELOAD="$LIBFAKEROOT" fakeroot pacman -r /rootfs -S --noconfirm \
     base \
     base-devel
 
 # install needed cli packages into the rootfs
-fakeroot pacman -r /rootfs -S --noconfirm \
+LD_PRELOAD="$LIBFAKEROOT" fakeroot pacman -r /rootfs -S --noconfirm \
     bash \
     bash-completion \
     curl \
@@ -59,19 +62,19 @@ fakeroot pacman -r /rootfs -S --noconfirm \
     yt-dlp
 
 # cleanup pacman cache
-fakeroot pacman -r /rootfs -Scc --noconfirm
+LD_PRELOAD="$LIBFAKEROOT" fakeroot pacman -r /rootfs -Scc --noconfirm
 rm -rf /rootfs/var/cache/pacman/pkg/*
 rm -rf /rootfs/var/lib/pacman/sync/*
 
 # set locale to en_US by default
 echo "en_US.UTF-8 UTF-8" > /rootfs/etc/locale.gen
-fakeroot chroot /rootfs locale-gen
+LD_PRELOAD="$LIBFAKEROOT" fakeroot chroot /rootfs locale-gen
 
 # extra subsystem configuration
 echo -e '\neval "$(starship init bash)"\neval "$(atuin init bash)"' >> /rootfs/etc/bash.bashrc
 
 # finalize subsystem build and create disk image
-fakeroot mkfs.erofs -zlz4hc,12 --fsid=subsystem /usr/lib/subsystem/subsystem.dsk /rootfs
+LD_PRELOAD="$LIBFAKEROOT" fakeroot mkfs.erofs -zlz4hc,12 -L "subsystem" /usr/lib/subsystem/subsystem.dsk /rootfs
 
 # cleanup
 pacman -Rns --noconfirm fakeroot
