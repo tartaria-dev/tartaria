66ae9c0a390d004181d7dda0f212046753cba8a6
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-01-27 16:40:28 -0800

    chore(looks): organize comments

--- build_files/06-finalize.sh
+++ build_files/06-finalize.sh
@@ -5,14 +5,12 @@ echo "::group::===========================> Finalize image build"
 
 set -ouex pipefail
 
-# Generate initramfs with dracut
-
-# set env vars and generate initramfs
+# generate initramfs with dracut
 KERNEL_VERSION="$(basename "$(find /usr/lib/modules -maxdepth 1 -type d | grep -v -E "\.img$" | tail -n 1)")"
 DRACUT_NO_XATTR=1 dracut --force --no-hostonly --reproducible --zstd --verbose --kver "$KERNEL_VERSION" "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 
-# Arrange filesystem for bootc and image-based systems
-# See https://bootc-dev.github.io/bootc/filesystem.html
+# arrange filesystem for bootc and image-based systems,
+# see https://bootc-dev.github.io/bootc/filesystem.html
 sed -i 's|^HOME=.*|HOME=/var/home|' "/etc/default/useradd"
 rm -rf /boot /home /root /usr/local /srv /mnt /var /usr/opt
 rm -rf /usr/lib/sysimage/log /usr/lib/sysimage/cache/pacman/pkg
