97809af748aecbe3b0aa3545ee34d086cd8cc630
machinoolio <mastersvanity@gmail.com>
2026-02-06 20:26:32 -0800

    fix(subsystem): properly handle /home

--- build_files/subsys/02-extra.sh
+++ build_files/subsys/02-extra.sh
@@ -9,4 +9,7 @@ set -ouex pipefail
 mkdir -p /etc/fish
 echo -e '\nif status is-interactive\n    starship init fish | source\n    atuin init fish | source\nend' >> /etc/fish/config.fish
 
+# compatiblity symlink for homedirs
+ln -sT /home /var/home
+
 echo "::endgroup::"
