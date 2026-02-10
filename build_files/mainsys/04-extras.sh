8fe9367c72de6d92a717b260b1d8e3af91490452
machinoolio <mastersvanity@gmail.com>
2026-02-09 17:16:21 -0800

    fix(build): read from stdin

--- build_files/mainsys/04-extras.sh
+++ build_files/mainsys/04-extras.sh
@@ -32,7 +32,7 @@ ln -sT /usr/share/tartaria/cherries/dot_config/niri/config.kdl /etc/niri/config.
 
 # create erofs image of subsystem store
 mkdir -p /usr/lib/subsystem-store
-mkfs.erofs -zlz4hc,12 -E all-fragments,fragdedupe=inode -L store --tar /usr/lib/subsystem-store/store.dsk /store.pipe > /dev/null
+cat /store.pipe | mkfs.erofs -zlz4hc,12 -E all-fragments,fragdedupe=inode -L store --tar /usr/lib/subsystem-store/store.dsk - > /dev/null
 
 # apply gschema overrides
 glib-compile-schemas /usr/share/glib-2.0/schemas
