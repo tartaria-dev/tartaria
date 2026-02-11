94d9c315d1eb455daa30b13fe78d627163bf7e9c
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-02-10 19:21:16 -0800

    fix(build): switch args around

--- build_files/03-subsystem.sh
+++ build_files/03-subsystem.sh
@@ -19,7 +19,7 @@ podman --root /store import ./output/subsystem.tar.zst subsystem:latest
 
 # compress store
 mkdir -p /usr/lib/subsystem-store
-mkfs.erofs -zlz4hc,12 -E all-fragments,fragdedupe=inode -L store /store /usr/lib/subsystem-store/store.dsk > /dev/null
+mkfs.erofs -zlz4hc,12 -E all-fragments,fragdedupe=inode -L store /usr/lib/subsystem-store/store.dsk /store > /dev/null
 
 # cleanup
 cd /
