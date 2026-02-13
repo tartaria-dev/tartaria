cb066f1e9fc15d6381e6201bb05510508d9c1fb5
machinoolio <mastersvanity@gmail.com>
2026-02-12 18:20:01 -0800

    chore(system): remove redundant modifications to bootc update timer

--- build_files/05-extras.sh
+++ build_files/05-extras.sh
@@ -12,10 +12,6 @@ sed -i 's/bgrt/red_loader/g' /usr/share/plymouth/plymouthd.defaults
 # disable uupd distrobox updates
 sed -i 's|uupd|& --disable-module-distrobox|' /usr/lib/systemd/system/uupd.service
 
-# reconfigure bootc auto update
-sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
-sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
-
 # install default icon theme
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme
 cd WhiteSur-icon-theme
