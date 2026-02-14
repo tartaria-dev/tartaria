bf49d4adcc2c2a1c94610bda9b6954b3df057266
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-02-13 21:02:51 -0800

    fix(build): correct executable path

--- build_files/03-subsystem.sh
+++ build_files/03-subsystem.sh
@@ -13,7 +13,7 @@ cp -rf /build/extra/* .
 # fetch host-spawn binary
 mkdir -p mkosi.extra/usr/libexec
 curl -fsSL https://github.com/1player/host-spawn/releases/download/v1.6.2/host-spawn-x86_64 -o mkosi.extra/usr/libexec/host-spawn
-chmod +x mkosi.extra/usr/bin/host-spawn
+chmod +x mkosi.extra/usr/libexec/host-spawn
 
 # build arch rootfs
 mkosi build
