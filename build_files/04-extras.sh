5f42ea1b71e87c6340916535fac9a96611e3a96c
machinoolio <mastersvanity@gmail.com>
2026-02-19 14:42:20 -0800

    chore(users): properly create greetd user

--- build_files/04-extras.sh
+++ build_files/04-extras.sh
@@ -6,9 +6,6 @@ set -ouex pipefail
 # set correct permissions on polkit rules dir
 chmod 750 /etc/polkit-1
 
-# manually add greetd user due to rebase issues
-useradd -M -G video,input -s /usr/bin/nologin greeter || true
-
 # configure useradd defaults
 sed -i 's|^HOME=.*|HOME=/var/home|' "/etc/default/useradd"
 
