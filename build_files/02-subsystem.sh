8c6e3259ee2e3a6cf64129a067476c5b6a9f8493
machinoolio <mastersvanity@gmail.com>
2026-02-23 18:19:25 -0800

    feat(subsystem): use privileged filesystem management for proper rootfs setup

--- build_files/02-subsystem.sh
+++ build_files/02-subsystem.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# install subsystem container store
+# install subsystem
 
 echo "::group::===========================> Install subsystem"
 
@@ -11,7 +11,6 @@ cd /workdir
 cp -rf /build/extra/* .
 
 # fetch host-spawn binary
-mkdir -p mkosi.extra/usr/libexec/
 wget -q https://github.com/1player/host-spawn/releases/download/v1.6.2/host-spawn-x86_64 -O mkosi.extra/usr/libexec/host-spawn
 chmod +x mkosi.extra/usr/libexec/host-spawn
 
@@ -20,7 +19,7 @@ mkosi build
 
 # compress rootfs
 mkdir -p /usr/lib/subsystem/rootfs
-mksquashfs /workdir/output/rootfs /usr/lib/subsystem/rootfs/base.dsk -comp zstd -Xcompression-level 19 -b 128K -noappend -always-use-fragments > /dev/null
+mkfs.erofs -zlz4hc,12 -E all-fragments,fragdedupe=inode -L rootfs /usr/lib/subsystem/rootfs/rootfs.dsk /workdir/output/rootfs >/dev/null
 
 # cleanup
 cd /
