8e6c267d2e515f85ee9c32923fb13abf2426b50e
machinoolio <mastersvanity@gmail.com>
2026-02-27 20:00:24 -0800

    fix(subsystem): append zshrc changes to correct file

--- system_files/etc/profile.d/containershell.sh
+++ system_files/etc/profile.d/containershell.sh
@@ -5,44 +5,6 @@
 SUBSYS_ID=$(echo -n "$(cat /etc/machine-id)$USER" | sha256sum | awk '{print $1}')
 readonly SUBSYS_ID
 
-# utilities
-first-time() {
-    if [[ -f ~/.config/subsystem/suppress-notice ]]; then
-        return
-    fi
-
-    cat <<EOF
-Hello there!
-
-Your shell is currently running inside a subsystem.
-Changes besides those to your homedir won't affect the host.
-Commands relating to container management (distrobox, docker, podman)
-are already linked into this subsystem.
-
-To see options for managing your subsystem, execute:
-ignition --help
-
-To see how to execute/link/unlink commands on/from the host, execute:
-synergy --help
-
-To suppress this lovely notice, execute:
-touch ~/.config/subsystem/suppress-notice
-EOF
-}
-
-errmsg() {
-    journalctl --user --no-pager -lxeu subsystem > "$HOME/.subsystem-failure"
-    cat <<EOF
-Oops,
-
-Your subsystem has failed to start.
-Logs have been stored in ~/.subsystem-failure.
-If logging out and back in doesn't fix things,
-manual investigation may be required.
-
-EOF
-}
-
 # check if the shell is interactive, if we are in a TTY, or if we are root
 if [[ $- != *i* ]]; then
     return
@@ -61,8 +23,16 @@ fi
 
 # check if subsystem is active and exec into subsystem, otherwise fail
 if [[ "$(systemctl --user is-failed subsystem)" == "active" ]]; then
-    first-time
     exec podman exec -u "$(id -u)" -it "subsystem-$SUBSYS_ID" /bin/zsh
 else
-    errmsg
+    journalctl --user --no-pager -lxeu subsystem > "$HOME/.subsystem-failure"
+    cat <<EOF
+Oops,
+
+Your subsystem has failed to start.
+Logs have been stored in ~/.subsystem-failure.
+If logging out and back in doesn't fix things,
+manual investigation may be required.
+
+EOF
 fi
