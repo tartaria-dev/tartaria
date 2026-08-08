8ad15ec6adb951138ace2a6f55e4b66cc1da1345
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-08-08 02:57:19 +0000

    fix(build): use builtin flags to disable sandbox

--- build_files/mainsys/04-extras.sh
+++ build_files/mainsys/04-extras.sh
@@ -7,9 +7,6 @@ set -ouex pipefail
 echo "uninitialized" > /etc/machine-id
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-# re-enable pacman network sandboxing (cachy)
-sed -i 's/^DisableSandboxNetwork/#DisableSandboxNetwork/' /etc/pacman.conf
-
 # set correct permissions on polkit rules dir
 chmod 750 /etc/polkit-1/rules.d
 chown -R root:polkitd /etc/polkit-1/rules.d
