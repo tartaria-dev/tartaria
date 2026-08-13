2bdffba423da59437bdadb1eb6161491fe5e3447
machinoolio <mastersvanity@gmail.com>
2026-08-13 11:00:36 -0700

    debug

--- build_files/mainsys/02-sysapps.sh
+++ build_files/mainsys/02-sysapps.sh
@@ -6,11 +6,11 @@ echo "::group::===========================> Install system apps"
 set -ouex pipefail
 
 # create sysapp dirs
-mkdir -p /usr/lib/flatpak-sysapps/dsk-src
+mkdir -p /usr/lib/flatpak-sysapps
 
 # store flatpaks
-# !!! package list also present in workflow files & install-flatpak-sysapps.service, modify those aswell if you modify the one below
-flatpak create-usb --installation=sysapps /usr/lib/flatpak-sysapps/dsk-src app.zen_browser.zen org.kde.ark org.gnome.baobab org.gnome.Decibels org.gnome.Calculator org.gnome.Calendar org.gnome.Music org.gnome.TextEditor org.gnome.Weather io.gitlab.adhami3310.Impression org.gnome.Loupe io.missioncenter.MissionCenter org.gnome.Papers org.gnome.World.Secrets org.gnome.Showtime org.mozilla.thunderbird >/dev/null
+ls /
+exit 1
 mkfs.erofs -zzstd,12 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /usr/lib/flatpak-sysapps/dsk-src >/dev/null
 
 # cleanup
