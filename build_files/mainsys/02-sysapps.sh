0bd88a7c50f5957e12d77453910f980f783c1f4a
machinoolio <mastersvanity@gmail.com>
2026-08-13 09:55:47 -0700

    fix(build): copy over sysapps to allow rw perms

--- build_files/mainsys/02-sysapps.sh
+++ build_files/mainsys/02-sysapps.sh
@@ -10,11 +10,10 @@ mkdir -p /usr/lib/flatpak-sysapps/dsk-src
 
 # store flatpaks
 # !!! package list also present in workflow files & install-flatpak-sysapps.service, modify those aswell if you modify the one below
-ls /tmp/sysapps
 flatpak create-usb --installation=sysapps /usr/lib/flatpak-sysapps/dsk-src app.zen_browser.zen org.kde.ark org.gnome.baobab org.gnome.Decibels org.gnome.Calculator org.gnome.Calendar org.gnome.Music org.gnome.TextEditor org.gnome.Weather io.gitlab.adhami3310.Impression org.gnome.Loupe io.missioncenter.MissionCenter org.gnome.Papers org.gnome.World.Secrets org.gnome.Showtime org.mozilla.thunderbird >/dev/null
 mkfs.erofs -zzstd,12 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /usr/lib/flatpak-sysapps/dsk-src >/dev/null
 
 # cleanup
-rm -rf /usr/lib/flatpak-sysapps/dsk-src /etc/flatpak/
+rm -rf /usr/lib/flatpak-sysapps/dsk-src /etc/flatpak/ /sysapps
 
 echo "::endgroup::"
