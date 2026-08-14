cb4d0ee67bb393650087b2e1e96cc3dba6ccfe41
machinoolio <mastersvanity@gmail.com>
2026-08-13 20:30:57 -0700

    fix(build): relocate bind mount destination

--- build_files/mainsys/02-sysapps.sh
+++ build_files/mainsys/02-sysapps.sh
@@ -6,14 +6,14 @@ echo "::group::===========================> Install system apps"
 set -ouex pipefail
 
 # create flatpak repo
-mkdir -p /sysapps
-flatpak create-usb /sysapps $(</etc/.sysapps-list) >/dev/null
+mkdir -p /sysapps-dsksrc
+flatpak --installation=sysapps create-usb /sysapps-dsksrc $(</etc/.sysapps-list) >/dev/null
 
 # compress flatpak repo
 mkdir -p /usr/lib/flatpak-sysapps
-mkfs.erofs -zzstd,9 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /sysapps >/dev/null
+mkfs.erofs -zzstd,9 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /sysapps-dsksrc >/dev/null
 
 # cleanup
-rm -rf /sysapps
+rm -rf /sysapps-dsksrc /etc/flatpak
 
 echo "::endgroup::"
