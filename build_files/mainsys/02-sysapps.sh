1d3366f8e512b95d38423c4f872eacfd089cd145
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-08-13 21:21:44 -0700

    fix(build): revert to previous approach with flatpak sysapp setup done (#46)
    * fix(build): revert to previous approach with flatpak sysapp setup done
    in build
    
    * fix(build): whoops
    
    * fix(build): move part of sysapp process into runner
    
    * fix(build): use correct filepath
    
    * fix(build): typo + do not use newlines in sysapps-list
    
    * fix(build): use correct filepath
    
    * fix(build): do not quote variable
    
    * fix(build): yeah no i'm just smart
    
    * fix(build): allow rw
    
    * fix(build): relocate bind mount destination
    
    * fix(build): ????
    
    * fix(build): I'M A GENIUS

---
+++ build_files/mainsys/02-sysapps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# commands for installing system apps (flatpaks)
+
+echo "::group::===========================> Install system apps"
+
+set -ouex pipefail
+
+# create flatpak repo
+mkdir -p /sysapps-dsksrc
+flatpak --installation=sysapps create-usb /sysapps-dsksrc $(</etc/.sysapps-list) >/dev/null
+
+# compress flatpak repo
+mkdir -p /usr/lib/flatpak-sysapps
+mkfs.erofs -zzstd,9 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /sysapps-dsksrc >/dev/null
+
+# cleanup
+rm -rf /sysapps-dsksrc /etc/flatpak
+
+echo "::endgroup::"
