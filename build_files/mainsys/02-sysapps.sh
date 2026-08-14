37e88175cd931a2da65c1b03a41ced783dcad5d6
machinoolio <mastersvanity@gmail.com>
2026-08-13 19:30:33 -0700

    fix(build): move part of sysapp process into runner

--- build_files/mainsys/02-sysapps.sh
+++ build_files/mainsys/02-sysapps.sh
@@ -5,33 +5,9 @@ echo "::group::===========================> Install system apps"
 
 set -ouex pipefail
 
-# define apps to install
-declare -a sysapps=(
-    app.zen_browser.zen
-    org.kde.ark
-    org.gnome.baobab
-    org.gnome.Decibels
-    org.gnome.Calculator
-    org.gnome.Calendar
-    org.gnome.Music
-    org.gnome.TextEditor
-    org.gnome.Weather
-    io.gitlab.adhami3310.Impression
-    org.gnome.Loupe
-    io.missioncenter.MissionCenter
-    org.gnome.Papers
-    org.gnome.World.Secrets
-    org.gnome.Showtime
-    org.mozilla.thunderbird
-) # note: package list also present in install-flatpak-sysapps.service
-
-# add flathub remote and install pkgs
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --no-deploy -y flathub "${sysapps[@]}" >/dev/null
-
 # create flatpak repo
 mkdir -p /sysapps
-flatpak create-usb /sysapps "${sysapps[@]}" >/dev/null
+flatpak create-usb /sysapps "$(<./system_files/etc/.sysapps-list)" >/dev/null
 
 # compress flatpak repo
 mkdir -p /usr/lib/flatpak-sysapps
