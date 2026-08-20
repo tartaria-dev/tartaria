f8d910c1a8abee4ae1051f8eb01ea6da7bbd27a9
machinoolio <mastersvanity@gmail.com>
2026-08-19 19:03:03 -0700

    feat(scripts): move flatpak system app installation to a script for simplicity

--- build_files/mainsys/02-sysapps.sh
+++ build_files/mainsys/02-sysapps.sh
@@ -13,11 +13,11 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 flatpak remote-modify --collection-id=org.flathub.Stable flathub
 
 # install apps
-flatpak install -y flathub $(</etc/bconfigs/system-apps) >/dev/null
+flatpak install -y flathub $(</etc/.sysapps.list) >/dev/null
 
 # setup flatpak repo
 curl -Lo /usr/lib/flatpak-sysapps/flathub.flatpakrepo https://flathub.org/repo/flathub.flatpakrepo
-flatpak create-usb /usr/lib/flatpak-sysapps/src $(</etc/bconfigs/system-apps) >/dev/null
+flatpak create-usb /usr/lib/flatpak-sysapps/src $(</etc/.sysapps.list) >/dev/null
 
 # compress flatpak repo
 mkfs.erofs -zzstd,9 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /usr/lib/flatpak-sysapps/src >/dev/null
