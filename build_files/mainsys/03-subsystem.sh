2e6f278550dd035256f6d2fcc746d23f67dcca78
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-09-04 08:58:51 -0700

    refactor(build): restructure containerfiles for simplicity (#62)
    * refactor(build): completely restructure containerfiles for simplicity
    
    * fix(build): install host-spawn into resulting rootfs instead
    
    * fix(build): remove var-tmp mount remnants

--- build_files/mainsys/03-subsystem.sh
+++ build_files/mainsys/03-subsystem.sh
@@ -7,10 +7,8 @@ echo "::group::===========================> Install subsystem"
 source /build/conf/00-functions
 set -ouex pipefail
 
-# fetch host-spawn binary
-mkdir -p /mkosi/mkosi.extra/usr/libexec
-retry wget -q https://github.com/1player/host-spawn/releases/download/v1.6.2/host-spawn-x86_64 -O /mkosi/mkosi.extra/usr/libexec/host-spawn
-chmod +x /mkosi/mkosi.extra/usr/libexec/host-spawn
+# create output dir
+mkdir -p /output
 
 # build arch rootfs
 if ! retry mkosi build --force --directory="/mkosi" --environment="IMAGE_VARIANT=$IMAGE_VARIANT" >/tmp/mkosi.log 2>&1; then
@@ -19,18 +17,23 @@ if ! retry mkosi build --force --directory="/mkosi" --environment="IMAGE_VARIANT
 fi
 
 # install extra pkgs
-if ! retry runuser -u builder -- bash -c "xargs -a /mkosi/conf/01-aur-pkgs yay -S --noconfirm --needed --root=/mkosi/output/image/" >/tmp/yay.log 2>&1; then
+if ! retry runuser -u builder -- bash -c "xargs -a /mkosi/conf/01-aur-pkgs yay -S --noconfirm --needed --root=/output/image/" >/tmp/yay.log 2>&1; then
     cat /tmp/build/yay.log
     exit 1
 fi
 
+# install host-spawn
+mkdir -p /output/image/usr/libexec
+retry wget -q https://github.com/1player/host-spawn/releases/download/v1.6.2/host-spawn-x86_64 -O /output/image/usr/libexec/host-spawn
+chmod +x /output/image/usr/libexec/host-spawn
+
 # compress rootfs
 mkdir -p /usr/lib/subsystem/rootfs
-retry mkfs.erofs -zzstd,5 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L rootfs /usr/lib/subsystem/rootfs/rootfs.dsk /mkosi/output/image >/dev/null
+retry mkfs.erofs -zzstd,5 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L rootfs /usr/lib/subsystem/rootfs/rootfs.dsk /output/image >/dev/null
 
 # cleanup
 pacman -Rns --noconfirm yay-bin
 userdel builder
-rm -rf /mkosi /tmp/mkosi.log /tmp/yay.log
+rm -rf /output /tmp/mkosi.log /tmp/yay.log
 
 echo "::endgroup::"
