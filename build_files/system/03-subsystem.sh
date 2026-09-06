bbe18b6d7abbf8811fe369437fbb8d64c7ef12c6
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2026-09-06 08:32:22 -0700

    feat(system): use host rootfs for subsystem and make subsystem ephemeral (#64)

---
+++ build_files/system/03-subsystem.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# install subsystem
+
+echo "::group::===========================> Install subsystem"
+
+# setup
+source /build/conf/00-functions
+set -ouex pipefail
+
+# install mkosi
+retry pacman -S --noconfirm --needed mkosi
+
+# create dirs
+mkdir -p /usr/lib/subsystem/segments
+
+# build dummy arch rootfs - provides minimal /var
+if ! retry mkosi build --force --directory="/mkosi" --environment="IMAGE_VARIANT=$IMAGE_VARIANT" >/tmp/mkosi.log 2>&1; then
+    cat /tmp/mkosi.log
+    exit 1
+fi
+
+# compress /etc
+retry mkfs.erofs -zzstd,19 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L etc /usr/lib/subsystem/segments/etc.dsk /etc >/dev/null
+
+# compress /var
+retry mkfs.erofs -zzstd,19 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L var /usr/lib/subsystem/segments/var.dsk /output/image/var >/dev/null
+
+# cleanup
+pacman -Rns --noconfirm mkosi
+rm -rf /output /tmp/mkosi.log
+
+echo "::endgroup::"
