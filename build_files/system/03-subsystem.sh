53bf0fcbf192af79fbb0386d835185b1933f82b3
machinoolio <mastersvanity@gmail.com>
2026-09-05 22:05:41 -0700

    fix(build): use correct log path

--- build_files/system/03-subsystem.sh
+++ build_files/system/03-subsystem.sh
@@ -12,7 +12,7 @@ mkdir -p /usr/lib/subsystem/segments
 
 # build dummy arch rootfs - provides minimal /var
 if ! retry mkosi build --force --directory="/mkosi" --environment="IMAGE_VARIANT=$IMAGE_VARIANT" >/tmp/mkosi.log 2>&1; then
-    cat /tmp/build/mkosi.log
+    cat /tmp/mkosi.log
     exit 1
 fi
 
@@ -23,6 +23,6 @@ retry mkfs.erofs -zzstd,19 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L 
 retry mkfs.erofs -zzstd,19 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L var /usr/lib/subsystem/segments/var.dsk /output/image/var >/dev/null
 
 # cleanup
-rm -rf /output
+rm -rf /output /tmp/mkosi.log
 
 echo "::endgroup::"
