d4c92df2a0bad807c2b0d5cd7ed2223b37fb8f77
machinoolio <mastersvanity@gmail.com>
2026-09-05 21:17:49 -0700

    fix(build): use dummy rootfs to provide minimal /var

--- build_files/system/02-sysapps.sh
+++ build_files/system/02-sysapps.sh
@@ -25,7 +25,7 @@ retry curl -Lo /usr/lib/flatpak-sysapps/flathub.flatpakrepo https://flathub.org/
 retry flatpak create-usb /usr/lib/flatpak-sysapps/src $(</etc/.sysapps.list) >/dev/null
 
 # compress flatpak repo
-retry mkfs.erofs -zzstd,9 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /usr/lib/flatpak-sysapps/src >/dev/null
+retry mkfs.erofs -zzstd,10 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L sysapps /usr/lib/flatpak-sysapps/flatpak-sysapps.dsk /usr/lib/flatpak-sysapps/src >/dev/null
 
 # cleanup
 rm -rf /usr/lib/flatpak-sysapps/src
--- build_files/system/03-subsystem.sh
+++ build_files/system/03-subsystem.sh
@@ -10,10 +10,19 @@ set -ouex pipefail
 # create dirs
 mkdir -p /usr/lib/subsystem/segments
 
+# build dummy arch rootfs - provides minimal /var
+if ! retry mkosi build --force --directory="/mkosi" --environment="IMAGE_VARIANT=$IMAGE_VARIANT" >/tmp/mkosi.log 2>&1; then
+    cat /tmp/build/mkosi.log
+    exit 1
+fi
+
 # compress /etc
-retry mkfs.erofs -zzstd,15 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L etc /usr/lib/subsystem/segments/etc.dsk /etc >/dev/null
+retry mkfs.erofs -zzstd,19 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L etc /usr/lib/subsystem/segments/etc.dsk /etc >/dev/null
 
 # compress /var
-retry mkfs.erofs -zzstd,15 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L var /usr/lib/subsystem/segments/var.dsk /var >/dev/null
+retry mkfs.erofs -zzstd,19 -C 65536 -E all-fragments,dedupe,fragdedupe=inode -L var /usr/lib/subsystem/segments/var.dsk /output/image/var >/dev/null
+
+# cleanup
+rm -rf /output
 
 echo "::endgroup::"
